957973aa8b33b3e9ab83c5e6651bd9c072fdd15a
b3atlesfan <74183294+b3atlesfan@users.noreply.github.com>
2020-11-13 06:51:20 -0500

    Initialize cm.topologyManager to prevent nil pointer error in device manager.

--- pkg/kubelet/cm/container_manager_windows.go
+++ pkg/kubelet/cm/container_manager_windows.go
@@ -109,9 +109,12 @@ func NewContainerManager(mountUtil mount.Interface, cadvisorInterface cadvisor.I
 		cadvisorInterface: cadvisorInterface,
 	}
 
-	klog.Infof("Creating device plugin manager: %t", devicePluginEnabled)
-	if devicePluginEnabled {
-		cm.deviceManager, err = devicemanager.NewManagerImpl(nil, cm.topologyManager)
+        	cm.topologyManager = topologymanager.NewFakeManager()
+
+        	klog.Infof("Creating device plugin manager: %t", devicePluginEnabled)
+        	if devicePluginEnabled {
+        	        cm.deviceManager, err = devicemanager.NewManagerImpl(nil, cm.topologyManager)
+         	        cm.topologyManager.AddHintProvider(cm.deviceManager)
 	} else {
 		cm.deviceManager, err = devicemanager.NewManagerStub()
 	}
